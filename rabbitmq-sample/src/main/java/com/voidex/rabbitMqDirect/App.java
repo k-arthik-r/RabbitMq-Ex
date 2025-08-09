4acf80f123ff24d363ffcc45c04512dfdab635e2
Karthik R <karthikclgid@gmail.com>
2025-08-09 20:23:32 +0530

    initial commit

---
+++ rabbitmq-sample/src/main/java/com/voidex/rabbitMqDirect/App.java
@@ -0,0 +1,10 @@
+package com.voidex.rabbitMqDirect;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
